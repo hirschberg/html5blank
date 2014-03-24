247c5fbed7ddb5ba81e70725ba9f0e3c653f3117
Felix Hirschberg <felix@diescouts.de>
2014-03-24 14:33:47 +0100

    Updated Plugin-Activation

--- functions.php
+++ functions.php
@@ -354,6 +354,7 @@ add_action('init', 'register_html5_menu'); // Add HTML5 Blank Menu
 add_action('init', 'create_post_type_html5'); // Add our HTML5 Blank Custom Post Type
 add_action('widgets_init', 'my_remove_recent_comments_style'); // Remove inline Recent Comment Styles from wp_head()
 add_action('init', 'html5wp_pagination'); // Add our HTML5 Pagination
+add_action( 'tgmpa_register', 'my_theme_register_required_plugins' );
 
 // Remove Actions
 remove_action('wp_head', 'feed_links_extra', 3); // Display the links to the extra feeds such as category feeds
